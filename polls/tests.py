471b9d9295510ef3ee8c1d5f101ae4ffef5cc9df
Kuenhwi <34573901+KueNiYam@users.noreply.github.com>
2020-12-18 15:22:26 +0900

    feat: part5 - model

--- polls/tests.py
+++ polls/tests.py
@@ -1,1 +1,34 @@
 # Create your tests here.
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from polls.models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        출시 날짜가 미래일 경우 False를 반환한다.
+        """
+        time = timezone.now() + datetime.timedelta(days=1)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        출시 날짜가 하루보다 오래되었을 경우 False를 반환한다.
+        """
+        time = timezone.now() - datetime.timedelta(days=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        출시 날짜가 하루 이내일 경우
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
