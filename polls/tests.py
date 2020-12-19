ed795c798edb5bf4a369d98728eba057425ed1a7
Kuenhwi <34573901+KueNiYam@users.noreply.github.com>
2020-12-20 03:48:13 +0900

    feat: part5 - view

--- polls/tests.py
+++ polls/tests.py
@@ -2,6 +2,7 @@
 import datetime
 
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from polls.models import Question
@@ -27,8 +28,88 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_recent_question(self):
         """
-        출시 날짜가 하루 이내일 경우
+        출시 날짜가 하루 이내일 경우 True를 반환한다.
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        Question이 없다면 적절한 메세지를 보여준다.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """
+        지난 Questions들이 인덱스 페이지에 보여진다.
+        """
+        create_question(question_text="Past question.", days=-1)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        """
+        미래의 Questions들은 인덱스 페이지에 보여지지 않는다.
+        """
+        create_question(question_text="Future question.", days=1)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        지난 Questions와 미래 Questions가 함께 있어도, 지난 것들만 보여준다.
+        """
+        create_question(question_text="Past question.", days=-1)
+        create_question(question_text="Future question.", days=1)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_questions(self):
+        """
+        Questions는 여러 개를 보여줄 수 있다.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
+
+
+class QuestionDetailViewTest(TestCase):
+    def test_future_question(self):
+        """
+        미래의 Question의 DetailView는 404 페이지를 반환해야 한다.
+        """
+        future_question = create_question(question_text='Future question.', days=1)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        지난 Question의 DetailView는 Question의 텍스트를 보여준다.
+        """
+        past_question = create_question(question_text='Past question.', days=-1)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
